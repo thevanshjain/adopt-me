e0784e0fa4e1ebf42a5e472d7b362508e7d4ea27
vanshjain2810 <vanshvasu2810@gmail.com>
2021-01-31 19:59:19 +0530

    implemented context

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,8 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
+
 class Details extends React.Component {
   state = { loading: true };
   //   constructor(props) {
@@ -11,7 +13,7 @@ class Details extends React.Component {
   //     };
   //   }
   componentDidMount() {
-    throw new error("lol");
+    // throw new error("lol");
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -35,8 +37,17 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${location}`}</h2>
-          <button>Adopt {name}</button>
-          <p>{description}</p>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                style={{ backgroundColor: theme }}
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
+          ;<p>{description}</p>
         </div>
       </div>
     );
--- src/Pet.js
+++ src/Pet.js
@@ -1,3 +1,4 @@
+import { Link } from "@reach/router";
 import React from "react";
 
 const Pet = ({ name, animal, breed, media, location, id }) => {
@@ -6,7 +7,7 @@ const Pet = ({ name, animal, breed, media, location, id }) => {
     hero = media[0].small;
   }
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -14,7 +15,7 @@ const Pet = ({ name, animal, breed, media, location, id }) => {
         <h1>{name}</h1>
         <h2>{` ${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
