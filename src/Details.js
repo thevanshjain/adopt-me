8a63b7ee8cd2dcaccd5ad0edffdae7d3eb67b653
vanshjain2810 <vanshvasu2810@gmail.com>
2021-01-31 19:12:40 +0530

    added error boundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 class Details extends React.Component {
   state = { loading: true };
   //   constructor(props) {
@@ -10,6 +11,7 @@ class Details extends React.Component {
   //     };
   //   }
   componentDidMount() {
+    throw new error("lol");
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -41,4 +43,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
